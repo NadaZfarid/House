fa6f2421338f6a59731ad50e82a36404b6b239fc
NadaZfarid <nadazfarid@gmail.com>
2023-05-04 07:28:03 +0200

    start

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
+import { PropertyCardComponent } from './Components/Property/property-card/property-card.component';
+import { PropertyListComponent } from './Components/Property/property-list/property-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    PropertyCardComponent,
+    PropertyListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
